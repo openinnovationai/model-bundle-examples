1b10d87a22aded61dff633408586361ab7e48339
Francesco Di Baldassarre <francesco.dibaldassarre@openinnovation.ai>
2024-08-06 10:03:14 +0400

    Reduce the size of the sample CNN

--- pytorch/model/model.py
+++ pytorch/model/model.py
@@ -21,19 +21,13 @@ class CNN(nn.Module):
         self.convlayer2 = nn.Sequential(
             nn.Conv2d(32, 64, 3), nn.BatchNorm2d(64), nn.ReLU(), nn.MaxPool2d(2)
         )
-        self.fc1 = nn.Linear(64 * 6 * 6, 600)
-        self.drop = nn.Dropout2d(0.25)
-        self.fc2 = nn.Linear(600, 120)
-        self.fc3 = nn.Linear(120, 10)
+        self.fc1 = nn.Linear(64 * 6 * 6, 10)
 
     def forward(self, x):
         x = self.convlayer1(x)
         x = self.convlayer2(x)
         x = x.view(-1, 64 * 6 * 6)
         x = self.fc1(x)
-        x = self.drop(x)
-        x = self.fc2(x)
-        x = self.fc3(x)
         return F.log_softmax(x, dim=1)
 
 
