d4c594496ca4c4186c84c0daac91f0dbe4bc404f
Syafiq Kamarul Azman <syafiqkamarulazman@gmail.com>
2025-08-06 16:17:46 +0400

    feat: update sklearn reqs, generator, and dir structure

--- sklearn/model.py
+++ sklearn/model.py
@@ -1,8 +1,10 @@
-from typing import Any
-import os
-import numpy as np
-import joblib
 import logging
+import os
+from typing import Any, Optional
+
+import joblib
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
 
 
 class Model:
@@ -11,17 +13,20 @@ class Model:
         config = kwargs["config"]
         model_metadata = config["model_metadata"]
         self._model_binary_dir = model_metadata["model_binary_dir"]
-        self._model = None
+        self._model: Optional[RandomForestRegressor] = None
 
     def load(self) -> None:
         model_binary_dir_path = os.path.join(
             str(self._data_dir), str(self._model_binary_dir)
         )
-        pkl_filepath = os.path.join(model_binary_dir_path, "data", "model.pkl")
-        logging.info(f"Loading model file {pkl_filepath}")
-        self._model = joblib.load(pkl_filepath)
+        joblib_filepath = os.path.join(model_binary_dir_path, "data", "model.joblib")
+        logging.info(f"Loading model file {joblib_filepath}")
+        self._model = joblib.load(joblib_filepath)
 
     def predict(self, model_input: Any) -> Any:
+        if self._model is None:
+            raise RuntimeError("Model not loaded")
+
         x = np.asarray(model_input)  # Convert the REST input to a numpy array
         result = self._model.predict(x)
         predictions = result.tolist()  # Convert the model output to a Python list
